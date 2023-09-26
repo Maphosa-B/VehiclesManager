494f3fbc1285475d25124e8776c362f649e0379c
BONGINKOSI MAPHOSA <maphosabonkosi.bm@gmail.com>
2023-09-26 22:07:10 +0200

    Add project files.

---
+++ VehiclesManager/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VehiclesManager.MvcApplication" Language="C#" %>
---
+++ VehiclesManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VehiclesManager.Startup))]
+namespace VehiclesManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
